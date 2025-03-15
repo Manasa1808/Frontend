83f0812b1b0b11c7631bf0d25bef8ce62f9c0074
Manasa1808 <manasa2004nm18@gmail.com>
2025-03-15 12:54:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import axios from "axios"
-// import { useState } from "react"
-// export default function App() {
-//   const [Username,setUsername]=useState("")
-//    function  handleSubmit(e){
-//       e.preventDefault()
-//       console.log(Username)
-//     }
-
-//     const [user,setUser] = useState("None")
-
-//     async function handleClick () {
-//      const response = await axios.get("http://localhost:3001/login",{username})
-//      console.log("success");
-//      setUser(response.data.title)
-//   }
-//     return (
-//      <>
-//      <form onSubmit={handleSubmit}>
-//       <label>Username</label>
-//       <input type="text" onChange={(e)=>{setUsername(e.target.value)}} />
-//       <input type="submit" value="submit"/>
-//      </form>
-//      </>
-//      )
-//    }
-
 import axios from "axios";
 import { useState } from "react";
 
